3f4dcb35ca692e8c001222ab043b654ccd693486
Nelson Kim <ssipflow@gmail.com>
2025-11-05 23:22:39 +0900

    Add BlackList

--- backend/src/main/java/com/ssipflow/backend/controller/UserController.java
+++ backend/src/main/java/com/ssipflow/backend/controller/UserController.java
@@ -5,20 +5,27 @@ import com.ssipflow.backend.dto.UserLogin;
 import com.ssipflow.backend.entity.User;
 import com.ssipflow.backend.jwt.JwtUtil;
 import com.ssipflow.backend.service.CustomUserDetailService;
+import com.ssipflow.backend.service.JwtBlacklistService;
 import com.ssipflow.backend.service.UserService;
 import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.naming.AuthenticationException;
-import java.util.HashMap;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -33,6 +40,7 @@ public class UserController {
     private final UserService userService;
     private final JwtUtil jwtUtil;
     private final CustomUserDetailService userDetailService;
+    private final JwtBlacklistService jwtBlacklistService;
 
     @GetMapping("")
     public ResponseEntity<List<User>> getAllUsers() {
@@ -57,7 +65,8 @@ public class UserController {
     public String login(@RequestBody UserLogin userLogin,
                         HttpServletResponse response) throws AuthenticationException {
         // authenticate with username, password
-        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userLogin.getUsername(), userLogin.getPassword()));
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userLogin.getUsername(),
+                userLogin.getPassword()));
 
         // loadUserByUsername for JWT payload information
         UserDetails userDetails = userDetailService.loadUserByUsername(userLogin.getUsername());
@@ -80,5 +89,40 @@ public class UserController {
         cookie.setPath("/");
         cookie.setMaxAge(0);
         response.addCookie(cookie);
+    }
+
+    @PostMapping("/logout/all")
+    public void logoutAll(
+            @RequestParam(required = false) String requestToken,
+            @CookieValue(value = "token", required = false) String cookieToken,
+            HttpServletRequest request, HttpServletResponse response) {
+        String token = null;
+        String bearerToken = request.getHeader("Authorization");
+        if (requestToken != null) {
+            token = requestToken;
+        } else if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            token = bearerToken.substring(7);
+        } else if (cookieToken != null) {
+            token = cookieToken;
+        }
+        Instant now = new Date().toInstant();
+        LocalDateTime expirationTime = now.atZone(ZoneId.systemDefault()).toLocalDateTime();
+
+        String username = jwtUtil.getUsername(token);
+        jwtBlacklistService.blacklistToken(token, expirationTime, username);
+
+        Cookie cookie = new Cookie("token", null);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        cookie.setMaxAge(0);    // Delete cookie
+        response.addCookie(cookie);
+    }
+
+    @PostMapping("token/validation")
+    @ResponseStatus(HttpStatus.OK)
+    public void jwtValidate(@RequestParam String token) {
+        if (!jwtUtil.validateToken(token)) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Invalid JWT");
+        }
     }
 }
